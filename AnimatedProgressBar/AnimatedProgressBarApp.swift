2f56cdb12ba2b8230efd393b827c7770d58da857
Irina Saf <irina.safronova0801@gmail.com>
2025-10-28 10:10:38 -0400

    Initial Commit

---
+++ AnimatedProgressBar/AnimatedProgressBarApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimatedProgressBarApp.swift
+//  AnimatedProgressBar
+//
+//  Created by Irina Saf on 2025-10-28.
+//
+
+import SwiftUI
+
+@main
+struct AnimatedProgressBarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
